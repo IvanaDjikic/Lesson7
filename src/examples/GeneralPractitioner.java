931f6c471ea0e68c359bf6d95801437aab9e513b
IvanaDjikic <ivdjikic@gmail.com>
2019-06-03 01:04:47 +0200

    general practitioner

---
+++ src/examples/GeneralPractitioner.java
@@ -0,0 +1,13 @@
+package examples;
+
+public class GeneralPractitioner extends Doctor {
+
+	public GeneralPractitioner(String name, int age) {
+		super(name, age);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void makeHouseCalls() {
+		System.out.println("I make house calls");
+	}
+}
