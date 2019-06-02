3c1b56e0f0768ddb10f0124ad2bfa51ee66dd71e
IvanaDjikic <ivdjikic@gmail.com>
2019-06-03 01:06:14 +0200

    surgeon

---
+++ src/examples/Surgron.java
@@ -0,0 +1,14 @@
+package examples;
+
+public class Surgron extends Doctor {
+
+	public Surgron(String name, int age) {
+		super(name, age);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void performSurgery() {
+		System.out.println("I perform surgery");
+	}
+
+}
